8b8b7b2b9994e33ce900acb167963462e38f3a42
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-24 14:05:40 -0300

    Registrando tabelas no admin

--- sectrans/admin.py
+++ sectrans/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#Registra os modelos no admin
+admin.site.register(ModelosEquipamento)
+admin.site.register(Empresas)
+admin.site.register(Redes)
+
