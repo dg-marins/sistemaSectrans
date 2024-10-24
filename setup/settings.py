885297b42bb8f8cc33a4f4b815b27199f646d558
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-24 13:44:24 -0300

    configuração do banco de dados

--- setup/settings.py
+++ setup/settings.py
@@ -74,11 +74,22 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sectrans',        
+        'USER': 'postgres',          
+        'PASSWORD': 'a5ztq3ej',        
+        'HOST': 'localhost',            
+        'PORT': '5433',
+        }
 }
 
 
