962e9e352ed242f259797636cee5bd8b25c2cbbb
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-24 21:36:06 -0300

    Lista as empresas de forma dinamica

--- sectrans/urls.py
+++ sectrans/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import selecionar_empresa
+from .views import selecionar_empresa, listar_empresas
 
 urlpatterns = [
      path('selecionar-empresa/', selecionar_empresa, name='selecionar_empresa'),
+     path('empresas/', listar_empresas, name='listar_empresas'),
 ]
--- sectrans/views.py
+++ sectrans/views.py
@@ -1,5 +1,10 @@
 # views.py
 from django.shortcuts import render
+from .models import Empresa
 
 def selecionar_empresa(request):
     return render(request, 'selecionar_empresa.html')
+
+def listar_empresas(request):
+    empresas = Empresa.objects.all().order_by('nome')
+    return render(request, 'selecionar_empresa.html', {'empresas': empresas})
