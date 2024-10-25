316992a4f34525df436eab4f4cb923d4c563964f
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-25 01:06:00 -0300

    configurado página index

--- sectrans/urls.py
+++ sectrans/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     path('', views.login_view, name='index'),
      path('selecionar-empresa/', views.selecionar_empresa, name='selecionar_empresa'),
      path('empresas/', views.listar_empresas, name='listar_empresas'),
      path('empresas/<int:empresa_id>/carros/', views.listar_carros, name='listar_carros'),
