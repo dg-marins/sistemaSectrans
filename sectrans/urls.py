c5d22e20164443558d7a834a3981114147761958
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-25 01:02:54 -0300

    Página basica de login e redirecionamentos criada

--- sectrans/urls.py
+++ sectrans/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
      path('selecionar-empresa/', views.selecionar_empresa, name='selecionar_empresa'),
      path('empresas/', views.listar_empresas, name='listar_empresas'),
      path('empresas/<int:empresa_id>/carros/', views.listar_carros, name='listar_carros'),
+     path('login/', views.login_view, name='login'),
+     path('logout/', views.listar_empresas, name='logout'),
 ]
--- sectrans/views.py
+++ sectrans/views.py
@@ -1,7 +1,10 @@
 # views.py
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Empresa, Carro, Modelo_Equipamento
 from django.http import JsonResponse
+
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def selecionar_empresa(request):
     return render(request, 'selecionar_empresa.html')
@@ -29,3 +32,20 @@ def listar_modelos(request, carro_id):
         modelos = Modelo_Equipamento.objects.filter(veiculo=carro).order_by('modelo').values('id', 'modelo')
         return JsonResponse({'modelos': list(modelos)})
 
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'Login realizado com sucesso!')
+            return redirect('listar_empresas')  # Redirecione para a página desejada após login bem-sucedido
+        else:
+            messages.error(request, 'Usuário ou senha incorretos. Tente novamente.')
+    
+    return render(request, 'login.html')
+
+def home_view(request):
+    return render(request, 'login.html')
