48c7f9546f7979024d3e46e3fb6f8fbc0b505577
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-26 14:11:49 -0300

    Remoção de métodos descontinuados

--- sectrans/views.py
+++ sectrans/views.py
@@ -5,25 +5,6 @@ from django.http import JsonResponse
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 
-def listar_carros(request, empresa_id):
-    empresa = get_object_or_404(Empresa, id=empresa_id)
-    
-    # Verificando se a requisição é AJAX
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        carros = Carro.objects.filter(empresa_id=empresa_id).order_by('nome').values('id', 'nome')
-        return JsonResponse({'carros': list(carros)})
-    
-    carros = Carro.objects.filter(empresa_id=empresa_id).order_by('nome')
-    return render(request, 'listar_carros.html', {'empresa': empresa, 'carros': carros})
-
-def listar_modelos(request, carro_id):
-    carro = get_object_or_404(Carro, id=carro_id)
-    
-    # Verificando se a requisição é AJAX
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        modelos = Modelo_Equipamento.objects.filter(veiculo=carro).order_by('modelo').values('id', 'modelo')
-        return JsonResponse({'modelos': list(modelos)})
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -42,3 +23,11 @@ def login_view(request):
 def pedido_midia(request):
     empresas = Empresa.objects.all().order_by('nome')
     return render(request, 'pedido_midia.html', {'empresas': empresas})
+
+def listar_modelos(request, carro_id):
+    carro = get_object_or_404(Carro, id=carro_id)
+    
+    # Verificando se a requisição é AJAX
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        modelos = Modelo_Equipamento.objects.filter(veiculo=carro).order_by('modelo').values('id', 'modelo')
+        return JsonResponse({'modelos': list(modelos)})
