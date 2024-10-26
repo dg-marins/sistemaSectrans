a94d9b1e7d236a90477fab945939b6506e98e60b
Douglas Marins <douglasmarinssouza@gmail.com>
2024-10-26 14:11:21 -0300

    Remoção de urla não utilizada

--- sectrans/urls.py
+++ sectrans/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
      path('', views.login_view, name='index'),
      path('login/', views.login_view, name='login'),
      path('pedido_midia/', views.pedido_midia, name='pedido_midia'),
-     path('empresas/<int:empresa_id>/carros/', views.listar_carros, name='listar_carros'),
      path('logout/', views.login_view, name='logout'),
 ]
