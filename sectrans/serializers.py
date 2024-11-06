9fc8125cafc80752c04365cb099b685d26baf586
Douglas Marins <douglasmarinssouza@gmail.com>
2024-11-06 00:09:54 -0300

    Alteração nos parametros da requisição para registrar videos

--- sectrans/serializers.py
+++ sectrans/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 from .models import Video
 
-class VideoSerializers(serializers.ModelSerializer):
+class VideoDataSerializer(serializers.Serializer):
+    video_file = serializers.CharField(max_length=255)
+    channel = serializers.IntegerField()
+    data_video = serializers.DateField()
+    hora_video = serializers.TimeField()
+    tamanho = serializers.IntegerField()
+    duracao = serializers.CharField(max_length=10)
+    path_arquivo = serializers.CharField(max_length=255)
 
-    class Meta:
-        model = Video
-        fields = '__all__'
+class VideoRequestSerializer(serializers.Serializer):
+    carro = serializers.IntegerField()
+    empresa = serializers.IntegerField()
+    servidor = serializers.IntegerField()
+    videos = VideoDataSerializer(many=True)
